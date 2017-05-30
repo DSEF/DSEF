5b6198c88b9177092ec5fabfd2739ddde742bdd1
Samuel Thomas <sgpthomas@gmail.com>
2017-05-30 18:36:31 +0000

    comments

--- postprocess/dynamic_shuffle_dirs.bash
+++ postprocess/dynamic_shuffle_dirs.bash
@@ -10,17 +10,15 @@ fi
 output_dir=$1
 
 #have client/system/trial ... want trial/system/client
-echo $(ls -d $output_dir/client*)
 for client_full in $(ls -d $output_dir/client*); do
     client=$(echo $client_full | awk -F"${output_dir}/" '{ print $2 }')
     echo $client
-    echo $(ls -dp ${client_full}/* | grep '/$')
     for system_full in $(ls -dp ${client_full}/* | grep '/$'); do
-	      system=$(echo $system_full | awk -F"${client_full}/" '{ print $2 }')
-	      echo "  "$system
-	      for trial_full in $(ls -dp ${system_full}/* | grep '/$'); do
-	          trial=$(echo $trial_full | awk -F"${system_full}/" '{ print $2 }')
-	          echo "    "$trial
+	system=$(echo $system_full | awk -F"${client_full}/" '{ print $2 }')
+	echo "  "$system
+	for trial_full in $(ls -dp ${system_full}/* | grep '/$'); do
+		trial=$(echo $trial_full | awk -F"${system_full}/" '{ print $2 }')
+	       	echo "    "$trial
 
 	          target_dir="$output_dir/$trial/$system/$client"
 	          mkdir -p $target_dir
@@ -28,6 +26,8 @@ for client_full in $(ls -d $output_dir/client*); do
 	      done
     done
 done
+
+echo "done shuffling"
 
 cd $output_dir
 tar -cjf clis.tar.bz2 client*
